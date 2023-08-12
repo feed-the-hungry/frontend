60832a359f46860c7dd8ba0466fb07a8abd2a21f
Bruno Arueira <contato@brunoarueira.com>
2023-08-12 19:31:45 -0300

    re-format files

--- src/services/feed_entries_service/index.ts
+++ src/services/feed_entries_service/index.ts
@@ -1,5 +1,5 @@
 import Fetch from './fetch';
 
 export {
-    Fetch
+  Fetch
 }
--- src/services/feeds_service/fetch.ts
+++ src/services/feeds_service/fetch.ts
@@ -1,16 +1,16 @@
 import { Feed } from '../../domain';
 
 export interface Fetch {
-	all(): Feed[];
+  all(): Feed[];
 }
 
 class FetchImpl implements Fetch {
-	all(): Feed[] {
-		return [
-			new Feed('0', 'Bruno Arueira', 'https://brunoarueira.com/feed.xml'),
-			new Feed('1', 'Hacker News', 'https://news.ycombinator.com/news.xml')
-		];
-	}
+  all(): Feed[] {
+    return [
+      new Feed('0', 'Bruno Arueira', 'https://brunoarueira.com/feed.xml'),
+      new Feed('1', 'Hacker News', 'https://news.ycombinator.com/news.xml')
+    ];
+  }
 }
 
 export default FetchImpl;
